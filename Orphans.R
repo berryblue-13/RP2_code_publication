cd33e15d6afdadd9afcdd6cde37af31f75e55718
SebastiaanM <s.j.molenaar@umail.leidenuniv.nl>
2026-07-11 16:32:35 +0200

    Code publication and instructions for use

---
+++ Orphans.R
@@ -0,0 +1,89 @@
+
+
+#Clearing global environment and loading necessary packages
+rm(list = ls())
+setwd("D:/Documents/Universiteit - Master/Research Project 2/GitHub/RP2_code_publication") #The full path to the folder with this script and the data should be specified between quotation brackets
+library(tidyverse)
+library(data.table)
+library(lubridate)
+library(DescTools)
+
+#Loading relevant files
+RepurposingDB <- read.delim("repurposing and type II for rstud with index.txt")
+EMRD <- read.delim("EMRD_for_rstud.txt")
+
+
+#Filtering repurposed drugs from the repurposingDB (Muizelaar et al., 2025)
+repurposed <- RepurposingDB %>%
+  filter(
+    repurposing == 1|type2_var == 1
+  )
+
+
+#Filtering orphans from EMRD (Bloem et al., 2025)
+EMRD_orphans <- EMRD %>%
+  filter(
+    OD_at_MA == "True"
+  )
+
+
+#Harmonisation of name formatting
+repurposed$name <- tolower(repurposed$name)
+
+EMRD_orphans$brand_name_current <- tolower(EMRD_orphans$brand_name_current)
+
+EMRD_orphans$brand_name_original <- tolower(EMRD_orphans$brand_name_original)
+
+
+#Filtering orphans from repurposed drugs based on brand name
+repurposed_orphans <- repurposed %>%
+  filter(
+    name %in% c(EMRD_orphans$brand_name_current, EMRD_orphans$brand_name_original)
+    )
+
+#Filtering orphans from repurposed drugs based on name of active substance. 
+#This list may contain false positives and should therefore be manually verified
+repurposed_orphans_substancebased <- repurposed %>%
+  filter(
+    active_substance %in% c(EMRD_orphans$act_substance_current, EMRD_orphans$act_substance_original),
+    !name %in% c(repurposed_orphans$name)
+    )
+
+
+#Preparing the list of orphan repurposing cases for export
+repurposed_orphans_export <- as.data.frame(repurposed_orphans)
+
+repurposed_orphans_export <- repurposed_orphans_export %>%
+  select(
+    name, 
+    active_substance, 
+    ma_date, 
+    type2_var, 
+    repurposing
+    ) %>%
+  mutate(
+    ma_date = dmy(ma_date)
+  )
+
+#Preparing the list of of substance-based matches for export
+substance_based_matches_export <- as.data.frame(repurposed_orphans_substancebased)
+
+substance_based_matches_export <- substance_based_matches_export %>%
+  select(
+    name, 
+    active_substance, 
+    ma_date, 
+    type2_var, 
+    repurposing
+  ) %>%
+  mutate(
+    ma_date = dmy(ma_date)
+  )
+
+#Export lists to folder
+write.csv(repurposed_orphans_export, file = "repurposed orphans.csv", row.names = FALSE)
+write.csv(substance_based_matches_export, file = "substance based matches.csv", row.names = FALSE)
+
+#Necessary warning messages
+writeLines("THE LIST CONTAINED IN SUBSTANCE BASED MATCHES MAY CONTAIN FALSE POSITIVE CASES AND SHOULD BE MANUALLY CHECKED FOR ORPHAN STATUS AND REPURPOSING CLASSIFICATION", "IMPORTANT READ ME.txt")
+cat("\n Script completed. \n IMPORTANT NOTE: THE LIST CONTAINED IN SUBSTANCE BASED MATCHES MAY CONTAIN FALSE POSITIVE CASES AND SHOULD BE MANUALLY CHECKED FOR ORPHAN STATUS AND REPURPOSING CLASSIFICATION")
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # RP2_code_publication
 This repository serves to provide insight for report readers into the code used in my project.
+
+## Getting Started and Structure
+
+1. **Create a folder on your computer**  
+   I called mine `Project`.  
+
+2. **Open a terminal** in that folder (right click > open in terminal).  
+
+3. **Clone this repository**  
+   - On the public repository, find the green button that says "< > Code" and copy the HTTPS link.
+   - In the terminal window you opened earlier, type "git clone", paste the link, hit enter.
+   - If this produces an error you may not have Git installed. In that case you should first run the following code first to install Git:
+
+```
+winget install --id Git.Git -e --source winget
+```
+
+4. **Open Orphans.R** and make sure to insert the full path to your project folder on line 5.  
+
+5. **Add data to the folder**  
+    - The data is not shared publicly for privacy reasons, please get in contact at _s.j.molenaar (at) umail.leidenuniv.nl_ or _am.pasmooij (at) cbg-meb.nl_.
+
+After this, your directory should look like this:
+
+---
+```
+Project/
+├── EMRD_for_Rstud.txt
+├── LICENSE
+├── Orphans.R
+└── README
+└── repurposing and type II for rstud with index.txt
+```
+
+6. **Source Orphans.R**. Before doing this, make sure all required packages are installed. Consult lines 6-9 of Orphans.R for this.
